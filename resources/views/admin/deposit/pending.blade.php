43e535081d58d813f5a6b0b5e2fe97fc48fd108f
emma ingurat <e.ingurat@bsmart-technology.com>
2021-06-27 23:20:43 +0300

    pending deposits datatable fix

--- resources/views/admin/deposit/pending.blade.php
+++ resources/views/admin/deposit/pending.blade.php
@@ -1,5 +1,43 @@
 @extends('master')
 
+<link rel="icon" type="image/png" sizes="16x16" href="images/favicon.png">
+<link href="{{ asset('vendor/datatables/css/jquery.dataTables.min.css') }}" rel="stylesheet">
+<link href="{{ asset('css/style.css') }}" rel="stylesheet">
+<link
+    href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&amp;family=Roboto:wght@100;300;400;500;700;900&amp;display=swap"
+    rel="stylesheet">
+
+@push('extra-js')
+    <script src="{{ asset('vendor/global/global.min.js') }}"></script>
+    <script src="{{ asset('vendor/bootstrap-select/dist/js/bootstrap-select.min.js') }}"></script>
+
+    <!-- Datatable -->
+    <script src="{{ asset('vendor/datatables/js/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('js/plugins-init/datatables.init.js') }}"></script>
+    <script src="{{ asset('js/custom.min.js') }}"></script>
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $('#tbl_pending_deposits').DataTable({
+                language: {
+                    emptyTable: "No pending deposits available",
+                    info: "Showing _START_ to _END_ of _TOTAL_ pending deposits",
+                    infoEmpty: "Showing 0 to 0 of 0 pending deposits",
+                    infoFiltered: "(filtered from _MAX_ total pending deposits)",
+                    infoPostFix: "",
+                    thousands: ",",
+                    lengthMenu: "Show _MENU_ pending deposits",
+                    loadingRecords: "<div class='loader slim'></div>",
+                    processing: "<div class='loader slim'></div>",
+                    search: "Search pending deposits:",
+                    zeroRecords: "No pending deposits match search criteria"
+                },
+            });
+
+        });
+
+    </script>
+@endpush
+
 @section('content')
 <div class="page-titles">
     <ol class="breadcrumb">
@@ -8,76 +46,76 @@
     </ol>
 </div>
 
-    <div class="content">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="card-header header-elements-inline">
-                        <h6 class="card-title font-weight-semibold">Pending deposit</h6>
-                    </div>
-                    <div class="">
-                        <table class="table datatable-show-all">
-                            <thead>
-                                <tr>
-                                    <th>S/N</th>
-                                    <th>Name</th>
-                                    <th>Amount</th>                                                                       
-                                    <th>Method</th>
-                                    <th>Ref</th>
-                                    <th>Charge</th>
-                                    <th>Created</th>
-                                    <th>Updated</th>
-                                    <th class="text-center">Action</th>    
-                                </tr>
-                            </thead>
-                            <tbody>
-                            @foreach($deposit as $k=>$val)
-                                <tr>
-                                    <td>{{++$k}}.</td>
-                                    <td><a href="{{url('admin/manage-user')}}/{{$val->user->id}}">{{$val->user->name}}</a></td>
-                                    <td>{{number_format($val->amount).$currency->name}}</td>
-                                    <td>{{$val->gateway['name']}}</td>
-                                    <td>{{$val->trx}}</td> 
-                                    <td>{{number_format($val->charge).$currency->name}}</td> 
-                                    <td>{{date("Y/m/d h:i:A", strtotime($val->created_at))}}</td>
-                                    <td>{{date("Y/m/d h:i:A", strtotime($val->updated_at))}}</td>
-                                    <td class="text-center">
-                                        <div class="list-icons">
-                                            <div class="dropdown">
-                                                <a href="#" class="list-icons-item" data-toggle="dropdown">
-                                                    <i class="icon-menu9"></i>
-                                                </a>
-                                                <div class="dropdown-menu dropdown-menu-right">
+<div class="row">
+    <div class="col-12">
+        <div class="card">
+            <div class="card-header">
+                <h4 class="card-title">Plans</h4>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table id="tbl_pending_deposits" class="display min-w850">
+                        <thead>
+                        <tr>
+                            <th>S/N</th>
+                            <th>Name</th>
+                            <th>Amount</th>
+                            <th>Method</th>
+                            <th>Ref</th>
+                            <th>Charge</th>
+                            <th>Created</th>
+                            <th>Updated</th>
+                            <th class="text-center">Action</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($deposit as $k=>$val)
+                            <tr>
+                                <td>{{++$k}}.</td>
+                                <td><a href="{{url('admin/manage-user')}}/{{$val->user->id}}">{{$val->user->name}}</a></td>
+                                <td>{{number_format($val->amount).$currency->name}}</td>
+                                <td>{{$val->gateway['name']}}</td>
+                                <td>{{$val->trx}}</td>
+                                <td>{{number_format($val->charge).$currency->name}}</td>
+                                <td>{{date("Y/m/d h:i:A", strtotime($val->created_at))}}</td>
+                                <td>{{date("Y/m/d h:i:A", strtotime($val->updated_at))}}</td>
+                                <td class="text-center">
+                                    <div class="list-icons">
+                                        <div class="dropdown">
+                                            <a href="#" class="list-icons-item" data-toggle="dropdown">
+                                                <i class="icon-menu9"></i>
+                                            </a>
+                                            <div class="dropdown-menu dropdown-menu-right">
                                                 <a class='dropdown-item' href="{{url('/')}}/admin/approvedeposit/{{$val->id}}"><i class="icon-thumbs-up3 mr-2"></i>Approve request</a>
                                                 <a class='dropdown-item' href="{{url('/')}}/admin/declinedeposit/{{$val->id}}"><i class="icon-thumbs-down3 mr-2"></i>Decline request</a>
-                                                    <a data-toggle="modal" data-target="#{{$val->id}}delete" class="dropdown-item"><i class="icon-bin2 mr-2"></i>Delete</a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </td>                   
-                                </tr>
-                                <div id="{{$val->id}}delete" class="modal fade" tabindex="-1">
-                                    <div class="modal-dialog">
-                                        <div class="modal-content">
-                                            <div class="modal-header">   
-                                                <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                            </div>
-                                            <div class="modal-body">
-                                                <h6 class="font-weight-semibold">Are you sure you want to delete this?</h6>
-                                            </div>
-                                            <div class="modal-footer">
-                                                <button type="button" class="btn btn-link" data-dismiss="modal">Close</button>
-                                                <a  href="{{url('/')}}/admin/deposit/delete/{{$val->id}}" class="btn bg-danger">Proceed</a>
+                                                <a data-toggle="modal" data-target="#{{$val->id}}delete" class="dropdown-item"><i class="icon-bin2 mr-2"></i>Delete</a>
                                             </div>
                                         </div>
                                     </div>
+                                </td>
+                            </tr>
+                            <div id="{{$val->id}}delete" class="modal fade" tabindex="-1">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <h6 class="font-weight-semibold">Are you sure you want to delete this?</h6>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-link" data-dismiss="modal">Close</button>
+                                            <a  href="{{url('/')}}/admin/deposit/delete/{{$val->id}}" class="btn bg-danger">Proceed</a>
+                                        </div>
+                                    </div>
                                 </div>
-                                @endforeach               
-                            </tbody>                    
-                        </table>
-                    </div>
+                            </div>
+                        @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
     </div>
+</div>
 @stop
