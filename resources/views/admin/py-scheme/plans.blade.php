d07201ff846fbdff67d75a662bc62aa08d45df8d
emma ingurat <e.ingurat@bsmart-technology.com>
2021-06-27 22:03:50 +0300

    py plan datatable fix

--- resources/views/admin/py-scheme/plans.blade.php
+++ resources/views/admin/py-scheme/plans.blade.php
@@ -1,34 +1,71 @@
 @extends('master')
+<link rel="icon" type="image/png" sizes="16x16" href="images/favicon.png">
+<link href="{{ asset('vendor/datatables/css/jquery.dataTables.min.css') }}" rel="stylesheet">
+<link href="{{ asset('css/style.css') }}" rel="stylesheet">
+<link
+    href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&amp;family=Roboto:wght@100;300;400;500;700;900&amp;display=swap"
+    rel="stylesheet">
+
+@push('extra-js')
+    <script src="{{ asset('vendor/global/global.min.js') }}"></script>
+    <script src="{{ asset('vendor/bootstrap-select/dist/js/bootstrap-select.min.js') }}"></script>
+
+    <!-- Datatable -->
+    <script src="{{ asset('vendor/datatables/js/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('js/plugins-init/datatables.init.js') }}"></script>
+    <script src="{{ asset('js/custom.min.js') }}"></script>
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $('#tbl_plans').DataTable({
+                language: {
+                    emptyTable: "No plans available",
+                    info: "Showing _START_ to _END_ of _TOTAL_ plans",
+                    infoEmpty: "Showing 0 to 0 of 0 plans",
+                    infoFiltered: "(filtered from _MAX_ total plans)",
+                    infoPostFix: "",
+                    thousands: ",",
+                    lengthMenu: "Show _MENU_ plans",
+                    loadingRecords: "<div class='loader slim'></div>",
+                    processing: "<div class='loader slim'></div>",
+                    search: "Search plans:",
+                    zeroRecords: "No plans match search criteria"
+                },
+            });
+
+        });
+
+    </script>
+@endpush
 
 @section('content')
-<div class="page-titles">
-    <ol class="breadcrumb">
-        <li class="breadcrumb-item"><a href="javascript:void(0)">Home</a></li>
-        <li class="breadcrumb-item active"><a href="#">Deposits</a></li>
-    </ol>
-</div>
+    <div class="page-titles">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="javascript:void(0)">Home</a></li>
+            <li class="breadcrumb-item active"><a href="#">Plans</a></li>
+        </ol>
+    </div>
 
-    <div class="content">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="card-header header-elements-inline">
-                        <h6 class="card-title font-weight-semibold">Plans</h6>
-                    </div>
-                    <div class="">
-                        <table class="table datatable-show-all">
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    <h4 class="card-title">Loans</h4>
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table id="tbl_plans" class="display min-w850">
                             <thead>
-                                <tr>
-                                    <th>S/N</th>
-                                    <th>Name</th>
-                                    <th>Monthly percent</th>                                                                       
-                                    <th>Minimum price</th>
-                                    <th>Maximum price</th>
-                                    <th>Status</th>
-                                    <th>Created</th>
-                                    <th>Updated</th>
-                                    <th class="text-center">Action</th>    
-                                </tr>
+                            <tr>
+                                <th>S/N</th>
+                                <th>Name</th>
+                                <th>Monthly percent</th>
+                                <th>Minimum price</th>
+                                <th>Maximum price</th>
+                                <th>Status</th>
+                                <th>Created</th>
+                                <th>Updated</th>
+                                <th class="text-center">Action</th>
+                            </tr>
                             </thead>
                             <tbody>
                             @foreach($plan as $k=>$val)
@@ -40,11 +77,11 @@
                                     <td>{{number_format($val->amount).$currency->name}}</td>
                                     <td>
                                         @if($val->status==0)
-                                            <span class="badge badge-danger">Disabled</span>
+                                            <span class="badge light badge-danger">Disabled</span>
                                         @elseif($val->status==1)
-                                            <span class="badge badge-success">Active</span> 
+                                            <span class="badge light badge-success">Active</span>
                                         @endif
-                                    </td>  
+                                    </td>
                                     <td>{{date("Y/m/d h:i:A", strtotime($val->created_at))}}</td>
                                     <td>{{date("Y/m/d h:i:A", strtotime($val->updated_at))}}</td>
                                     <td class="text-center">
@@ -54,14 +91,16 @@
                                                     <i class="icon-menu9"></i>
                                                 </a>
                                                 <div class="dropdown-menu dropdown-menu-right">
-                                                    <a class='dropdown-item' href="{{url('/')}}/admin/py-plan/{{$val->id}}"><i class="icon-pencil7 mr-2"></i>Edit</a>
+                                                    <a class='dropdown-item'
+                                                       href="{{url('/')}}/admin/py-plan/{{$val->id}}"><i
+                                                            class="icon-pencil7 mr-2"></i>Edit</a>
                                                 </div>
                                             </div>
                                         </div>
-                                    </td>                   
+                                    </td>
                                 </tr>
-                                @endforeach               
-                            </tbody>                    
+                            @endforeach
+                            </tbody>
                         </table>
                     </div>
                 </div>
