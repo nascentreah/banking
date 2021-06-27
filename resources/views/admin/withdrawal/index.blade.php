053623dedefbf33c43731fce83ae1cfb96fef4b2
emma ingurat <e.ingurat@bsmart-technology.com>
2021-06-28 01:12:28 +0300

    unpaid withdraws datatable fix

--- resources/views/admin/withdrawal/index.blade.php
+++ resources/views/admin/withdrawal/index.blade.php
@@ -39,98 +39,109 @@
 @endpush
 
 @section('content')
-<div class="page-titles">
-    <ol class="breadcrumb">
-        <li class="breadcrumb-item"><a href="javascript:void(0)">Home</a></li>
-        <li class="breadcrumb-item active"><a href="#">Withdraws</a></li>
-    </ol>
-</div>
+    <div class="page-titles">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="javascript:void(0)">Home</a></li>
+            <li class="breadcrumb-item active"><a href="#">Withdraws</a></li>
+        </ol>
+    </div>
 
-<div class="row">
-    <div class="col-12">
-        <div class="card">
-            <div class="card-header">
-                <h4 class="card-title">Withdraw logs</h4>
-            </div>
-            <div class="card-body">
-                <div class="table-responsive">
-                    <table id="tbl_withdraws" class="display min-w850">
-                        <thead>
-                        <tr>
-                            <th>S/N</th>
-                            <th>Name</th>
-                            <th>Amount</th>
-                            <th>Method</th>
-                            <th>Details</th>
-                            <th>Status</th>
-                            <th>Created</th>
-                            <th>Updated</th>
-                            <th class="text-center">Action</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        @foreach($withdraw as $k=>$val)
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    <h4 class="card-title">Withdraw logs</h4>
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table id="tbl_withdraws" class="display min-w850">
+                            <thead>
                             <tr>
-                                <td>{{++$k}}.</td>
-                                <td><a href="{{url('admin/manage-user')}}/{{$val->user->id}}">{{$val->user->name}}</a></td>
-                                <td>{{number_format($val->amount).$currency->name}}</td>
-                                <td>@if($val->coin_id=='bank')
-                                        bank
-                                    @else
-                                        {{$val->wallet->method}}
-                                    @endif
-                                </td>
-                                <td>{{$val->details}}</td>
-                                <td>
-                                    @if($val->status==0)
-                                        <span class="badge light badge-danger">Unpaid</span>
-                                    @elseif($val->status==1)
-                                        <span class="badge light badge-success">Paid</span>
-                                    @elseif($val->status==2)
-                                        <span class="badge light badge-info">Declined</span>
-                                    @endif
-                                </td>
-                                <td>{{date("Y/m/d h:i:A", strtotime($val->created_at))}}</td>
-                                <td>{{date("Y/m/d h:i:A", strtotime($val->updated_at))}}</td>
-                                <td class="text-center">
-                                    <div class="list-icons">
-                                        <div class="dropdown">
-                                            <a href="#" class="list-icons-item" data-toggle="dropdown">
-                                                <i class="icon-menu9"></i>
-                                            </a>
-                                            <div class="dropdown-menu dropdown-menu-right">
-                                                @if($val->status==0)
-                                                    <a class='dropdown-item' href="{{url('/')}}/admin/approvewithdraw/{{$val->id}}"><i class="icon-thumbs-up3 mr-2"></i>Approve request</a>
-                                                    <a class='dropdown-item' href="{{url('/')}}/admin/declinewithdraw/{{$val->id}}"><i class="icon-thumbs-down3 mr-2"></i>Decline request</a>
-                                                @endif
-                                                <a data-toggle="modal" data-target="#{{$val->id}}delete" class="dropdown-item"><i class="icon-bin2 mr-2"></i>Delete</a>
+                                <th>S/N</th>
+                                <th>Name</th>
+                                <th>Amount</th>
+                                <th>Method</th>
+                                <th>Details</th>
+                                <th>Status</th>
+                                <th>Created</th>
+                                <th>Updated</th>
+                                <th class="text-center">Action</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($withdraw as $k=>$val)
+                                <tr>
+                                    <td>{{++$k}}.</td>
+                                    <td>
+                                        <a href="{{url('admin/manage-user')}}/{{$val->user->id}}">{{$val->user->name}}</a>
+                                    </td>
+                                    <td>{{number_format($val->amount).$currency->name}}</td>
+                                    <td>@if($val->coin_id=='bank')
+                                            bank
+                                        @else
+                                            {{$val->wallet->method}}
+                                        @endif
+                                    </td>
+                                    <td>{{$val->details}}</td>
+                                    <td>
+                                        @if($val->status==0)
+                                            <span class="badge light badge-danger">Unpaid</span>
+                                        @elseif($val->status==1)
+                                            <span class="badge light badge-success">Paid</span>
+                                        @elseif($val->status==2)
+                                            <span class="badge light badge-info">Declined</span>
+                                        @endif
+                                    </td>
+                                    <td>{{date("Y/m/d h:i:A", strtotime($val->created_at))}}</td>
+                                    <td>{{date("Y/m/d h:i:A", strtotime($val->updated_at))}}</td>
+                                    <td class="text-center">
+                                        <div class="list-icons">
+                                            <div class="dropdown">
+                                                <a href="#" class="list-icons-item" data-toggle="dropdown">
+                                                    <i class="icon-menu9"></i>
+                                                </a>
+                                                <div class="dropdown-menu dropdown-menu-right">
+                                                    @if($val->status==0)
+                                                        <a class='dropdown-item'
+                                                           href="{{url('/')}}/admin/approvewithdraw/{{$val->id}}"><i
+                                                                class="icon-thumbs-up3 mr-2"></i>Approve request</a>
+                                                        <a class='dropdown-item'
+                                                           href="{{url('/')}}/admin/declinewithdraw/{{$val->id}}"><i
+                                                                class="icon-thumbs-down3 mr-2"></i>Decline request</a>
+                                                    @endif
+                                                    <a data-toggle="modal" data-target="#{{$val->id}}delete"
+                                                       class="dropdown-item"><i class="icon-bin2 mr-2"></i>Delete</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <div id="{{$val->id}}delete" class="modal fade" tabindex="-1">
+                                    <div class="modal-dialog">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <button type="button" class="close" data-dismiss="modal">&times;
+                                                </button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <h6 class="font-weight-semibold">Are you sure you want to delete
+                                                    this?</h6>
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-link" data-dismiss="modal">Close
+                                                </button>
+                                                <a href="{{url('/')}}/admin/withdraw/delete/{{$val->id}}"
+                                                   class="btn bg-danger">Proceed</a>
                                             </div>
                                         </div>
                                     </div>
-                                </td>
-                            </tr>
-                            <div id="{{$val->id}}delete" class="modal fade" tabindex="-1">
-                                <div class="modal-dialog">
-                                    <div class="modal-content">
-                                        <div class="modal-header">
-                                            <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                        </div>
-                                        <div class="modal-body">
-                                            <h6 class="font-weight-semibold">Are you sure you want to delete this?</h6>
-                                        </div>
-                                        <div class="modal-footer">
-                                            <button type="button" class="btn btn-link" data-dismiss="modal">Close</button>
-                                            <a  href="{{url('/')}}/admin/withdraw/delete/{{$val->id}}" class="btn bg-danger">Proceed</a>
-                                        </div>
-                                    </div>
                                 </div>
-                            </div>
-                        @endforeach
-                        </tbody>
-                    </table>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @stop
