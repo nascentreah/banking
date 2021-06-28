fccbd2d8d536ba2e7ec57ba1c831ef831bc76b42
emma ingurat <e.ingurat@bsmart-technology.com>
2021-06-28 13:10:03 +0300

    other bank transfers datatable fix

--- resources/views/admin/transfer/other-bank.blade.php
+++ resources/views/admin/transfer/other-bank.blade.php
@@ -1,59 +1,99 @@
 @extends('master')
 
-@section('content')
-<div class="page-titles">
-    <ol class="breadcrumb">
-        <li class="breadcrumb-item"><a href="javascript:void(0)">Home</a></li>
-        <li class="breadcrumb-item active"><a href="#">Other banks</a></li>
-    </ol>
-</div>
+<link rel="icon" type="image/png" sizes="16x16" href="images/favicon.png">
+<link href="{{ asset('vendor/datatables/css/jquery.dataTables.min.css') }}" rel="stylesheet">
+<link href="{{ asset('css/style.css') }}" rel="stylesheet">
+<link
+    href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&amp;family=Roboto:wght@100;300;400;500;700;900&amp;display=swap"
+    rel="stylesheet">
 
-    <div class="content">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="card-header header-elements-inline">
-                        <h6 class="card-title font-weight-semibold">Transfer logs</h6>
-                    </div>
-                    <div class="">
-                        <table class="table datatable-show-all">
+@push('extra-js')
+    <script src="{{ asset('vendor/global/global.min.js') }}"></script>
+    <script src="{{ asset('vendor/bootstrap-select/dist/js/bootstrap-select.min.js') }}"></script>
+
+    <!-- Datatable -->
+    <script src="{{ asset('vendor/datatables/js/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('js/plugins-init/datatables.init.js') }}"></script>
+    <script src="{{ asset('js/custom.min.js') }}"></script>
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $('#tbl_transfer_logs').DataTable({
+                language: {
+                    emptyTable: "No transfers available",
+                    info: "Showing _START_ to _END_ of _TOTAL_ transfers",
+                    infoEmpty: "Showing 0 to 0 of 0 transfers",
+                    infoFiltered: "(filtered from _MAX_ total transfers)",
+                    infoPostFix: "",
+                    thousands: ",",
+                    lengthMenu: "Show _MENU_ transfers",
+                    loadingRecords: "<div class='loader slim'></div>",
+                    processing: "<div class='loader slim'></div>",
+                    search: "Search transfers:",
+                    zeroRecords: "No transfers match search criteria"
+                },
+            });
+
+        });
+
+    </script>
+@endpush
+
+@section('content')
+    <div class="page-titles">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="javascript:void(0)">Home</a></li>
+            <li class="breadcrumb-item active"><a href="#">Other banks</a></li>
+        </ol>
+    </div>
+
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    <h4 class="card-title">Transfer Logs</h4>
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table id="tbl_transfer_logs" class="display min-w850">
                             <thead>
-                                <tr>
-                                    <th>S/N</th>
-                                    <th>Ref</th>
-                                    <th>Name</th>
-                                    <th>Amount</th>                                                                       
-                                    <th>Status</th>
-                                    <th>Type</th>
-                                    <th>Details</th>
-                                    <th>Created</th>
-                                    <th>Updated</th>
-                                    <th class="text-center">Action</th>    
-                                </tr>
+                            <tr>
+                                <th>S/N</th>
+                                <th>Ref</th>
+                                <th>Name</th>
+                                <th>Amount</th>
+                                <th>Status</th>
+                                <th>Type</th>
+                                <th>Details</th>
+                                <th>Created</th>
+                                <th>Updated</th>
+                                <th class="text-center">Action</th>
+                            </tr>
                             </thead>
                             <tbody>
                             @foreach($transfer as $k=>$val)
                                 <tr>
                                     <td>{{++$k}}.</td>
                                     <td>{{$val->ref_id}}</td>
-                                    <td><a href="{{url('admin/manage-user')}}/{{$val->user->id}}">{{$val->user->name}}</a></td>
+                                    <td>
+                                        <a href="{{url('admin/manage-user')}}/{{$val->user->id}}">{{$val->user->name}}</a>
+                                    </td>
                                     <td>{{number_format($val->amount).$currency->name}}</td>
                                     <td>
                                         @if($val->status==0)
-                                            <span class="badge badge-danger">Pending</span>
+                                            <span class="badge light badge-danger">Pending</span>
                                         @elseif($val->status==1)
-                                            <span class="badge badge-success">Successful</span> 
+                                            <span class="badge light badge-success">Successful</span>
                                         @endif
-                                    </td>   
+                                    </td>
                                     <td>
                                         @if($val->type==0)
-                                            <span class="badge badge-danger">Credit</span>
+                                            <span class="badge light badge-danger">Credit</span>
                                         @elseif($val->type==1)
-                                            <span class="badge badge-success">Debit</span> 
+                                            <span class="badge light badge-success">Debit</span>
                                         @endif
-                                    </td> 
-                                    <td>{{$val->details}}</td> 
-                                    <td>{{date("Y/m/d", strtotime($val->created_at))}}</td>  
+                                    </td>
+                                    <td>{{$val->details}}</td>
+                                    <td>{{date("Y/m/d", strtotime($val->created_at))}}</td>
                                     <td>{{date("Y/m/d h:i:A", strtotime($val->updated_at))}}</td>
                                     <td class="text-center">
                                         <div class="list-icons">
@@ -63,32 +103,39 @@
                                                 </a>
                                                 <div class="dropdown-menu dropdown-menu-right">
                                                     @if($val->status==0)
-                                                        <a class='dropdown-item' href="{{url('/')}}/admin/app-otherbank/{{$val->id}}"><i class="icon-thumbs-up3 mr-2"></i>Approve</a>
+                                                        <a class='dropdown-item'
+                                                           href="{{url('/')}}/admin/app-otherbank/{{$val->id}}"><i
+                                                                class="icon-thumbs-up3 mr-2"></i>Approve</a>
                                                     @endif
-                                                    <a data-toggle="modal" data-target="#{{$val->id}}delete" class="dropdown-item"><i class="icon-bin2 mr-2"></i>Delete</a>
+                                                    <a data-toggle="modal" data-target="#{{$val->id}}delete"
+                                                       class="dropdown-item"><i class="icon-bin2 mr-2"></i>Delete</a>
                                                 </div>
                                             </div>
                                         </div>
-                                    </td>                   
+                                    </td>
                                 </tr>
                                 <div id="{{$val->id}}delete" class="modal fade" tabindex="-1">
                                     <div class="modal-dialog">
                                         <div class="modal-content">
-                                            <div class="modal-header">   
-                                                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                            <div class="modal-header">
+                                                <button type="button" class="close" data-dismiss="modal">&times;
+                                                </button>
                                             </div>
                                             <div class="modal-body">
-                                                <h6 class="font-weight-semibold">Are you sure you want to delete this?</h6>
+                                                <h6 class="font-weight-semibold">Are you sure you want to delete
+                                                    this?</h6>
                                             </div>
                                             <div class="modal-footer">
-                                                <button type="button" class="btn btn-link" data-dismiss="modal">Close</button>
-                                                <a  href="{{url('/')}}/admin/other-bank/delete/{{$val->id}}" class="btn bg-danger">Proceed</a>
+                                                <button type="button" class="btn btn-link" data-dismiss="modal">Close
+                                                </button>
+                                                <a href="{{url('/')}}/admin/other-bank/delete/{{$val->id}}"
+                                                   class="btn bg-danger">Proceed</a>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                                @endforeach               
-                            </tbody>                    
+                            @endforeach
+                            </tbody>
                         </table>
                     </div>
                 </div>
