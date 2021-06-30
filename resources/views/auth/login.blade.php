f8c79efe83bb086edb88d472fdc4ba41b9dec18d
emma ingurat <e.ingurat@bsmart-technology.com>
2021-06-30 23:18:51 +0300

    sidebar fixes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,7 +6,7 @@
             <a href="index.html"><img src="images/logo-full.png" alt=""></a>
         </div>
         <h4 class="text-center mb-4 text-white">Sign in your account</h4>
-        <form method="POST" action="{{ route('login') }}">
+        <form method="POST" action="{{route('submitlogin')}}">
             @csrf
             <div class="form-group">
                 <label for="email" class="mb-1 text-white"><strong>Email</strong></label>
