8f7381cbbd91a8b67199eebdc6a541bf65586c10
emma ingurat <e.ingurat@bsmart-technology.com>
2021-07-24 01:05:46 +0300

    added chart

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -1,11 +1,49 @@
 @extends('master')
 
+<link rel="icon" type="image/png" sizes="16x16" href="images/favicon.png">
+<link href="{{ asset('vendor/datatables/css/jquery.dataTables.min.css') }}" rel="stylesheet">
+<link href="{{ asset('css/style.css') }}" rel="stylesheet">
+<link
+    href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&amp;family=Roboto:wght@100;300;400;500;700;900&amp;display=swap"
+    rel="stylesheet">
+
+@push('extra-js')
+    <script src="{{ asset('vendor/global/global.min.js') }}"></script>
+    <script src="{{ asset('vendor/bootstrap-select/dist/js/bootstrap-select.min.js') }}"></script>
+
+    <!-- Datatable -->
+    <script src="{{ asset('vendor/datatables/js/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('js/plugins-init/datatables.init.js') }}"></script>
+    <script src="{{ asset('js/custom.min.js') }}"></script>
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $('#tbl_transactions').DataTable({
+                language: {
+                    emptyTable: "No users available",
+                    info: "Showing _START_ to _END_ of _TOTAL_ users",
+                    infoEmpty: "Showing 0 to 0 of 0 users",
+                    infoFiltered: "(filtered from _MAX_ total users)",
+                    infoPostFix: "",
+                    thousands: ",",
+                    lengthMenu: "Show _MENU_ users",
+                    loadingRecords: "<div class='loader slim'></div>",
+                    processing: "<div class='loader slim'></div>",
+                    search: "Search users:",
+                    zeroRecords: "No users match search criteria"
+                },
+            });
+
+        });
+
+    </script>
+@endpush
+
 @section('content')
 
     <div class="page-titles">
         <ol class="breadcrumb">
             <li class="breadcrumb-item"><a href="javascript:void(0)">Home</a></li>
-            <li class="breadcrumb-item active"><a href="#">Deposits</a></li>
+            <li class="breadcrumb-item active"><a href="#">Dashboard</a></li>
         </ol>
     </div>
     <div class="row">
@@ -148,5 +186,49 @@
             </div>
         </div>
     </div>
+
+    <div class="row">
+        <div class="col-9">
+            <div class="card">
+                <div class="card-header">
+                    <h4 class="card-title">Transactions</h4>
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     
+    <div class="row">
+        <div class="col-9">
+            <div class="card">
+                <div class="card-header">
+                    <h4 class="card-title">Transactions</h4>
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table id="tbl_transactions" class="display min-w850">
+                            <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Amount</th>
+                                <th>Plan</th>
+                                <th>Monthly percent</th>
+                                <th>Profit</th>
+                                <th>Created</th>
+                                <th class="text-center">Action</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @stop
