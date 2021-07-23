b670c3f42fbe289de2f18ec0478fc650b39a1de3
emma ingurat <e.ingurat@bsmart-technology.com>
2021-07-24 00:35:52 +0300

    admin dashboard fixes

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -2,212 +2,151 @@
 
 @section('content')
 
-<div class="page-titles">
-    <ol class="breadcrumb">
-        <li class="breadcrumb-item"><a href="javascript:void(0)">Home</a></li>
-        <li class="breadcrumb-item active"><a href="#">Deposits</a></li>
-    </ol>
-</div>
+    <div class="page-titles">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="javascript:void(0)">Home</a></li>
+            <li class="breadcrumb-item active"><a href="#">Deposits</a></li>
+        </ol>
+    </div>
+    <div class="row">
 
-<div class="content">
-<div class="row">
-  <div class="col-md-4">
-    <div class="card border-left-3 border-left-violet rounded-left-0">
-      <div class="card-body">
-        <div class="d-sm-flex align-item-sm-center flex-sm-nowrap">
-          <div>
-            <h6 class="font-weight-semibold">Users</h6>
-            <ul class="list list-unstyled mb-0">
-              <li>Active users: <span class="font-weight-semibold text-default">#{{$activeusers}}</span></li>
-              <li>Blocked users: <span class="font-weight-semibold text-default">#{{$blockedusers}}</span></li>
-            </ul>
-          </div>
+        <div class="col-xl-3 col-lg-6 col-sm-6">
+            <div class="widget-stat card bg-danger">
+                <div class="card-body  p-4">
+                    <div class="media">
+									<span class="mr-3">
+										<i class="flaticon-381-calendar-1"></i>
+									</span>
+                        <div class="media-body text-white text-right">
+                            <p class="mb-1">Deactivated Users</p>
+                            <h3 class="text-white">{{ $blockedusers }}</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-3 col-lg-6 col-sm-6">
+            <div class="widget-stat card bg-success">
+                <div class="card-body p-4">
+                    <div class="media">
+									<span class="mr-3">
+										<i class="flaticon-381-diamond"></i>
+									</span>
+                        <div class="media-body text-white text-right">
+                            <p class="mb-1">Active Users</p>
+                            <h3 class="text-white">{{ $activeusers }}</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-3 col-lg-6 col-sm-6">
+            <div class="widget-stat card bg-info">
+                <div class="card-body p-4">
+                    <div class="media">
+									<span class="mr-3">
+										<i class="flaticon-381-heart"></i>
+									</span>
+                        <div class="media-body text-white text-right">
+                            <p class="mb-1">Total Deposits</p>
+                            <h3 class="text-white">{{ $totalbdeposit }}</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-3 col-lg-6 col-sm-6">
+            <div class="widget-stat card bg-primary">
+                <div class="card-body p-4">
+                    <div class="media">
+									<span class="mr-3">
+										<i class="flaticon-381-user-7"></i>
+									</span>
+                        <div class="media-body text-white text-right">
+                            <p class="mb-1">Total Withdraws</p>
+                            <h3 class="text-white">{{ $totalwd }}</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-          <div class="text-sm-right mb-0 mt-3 mt-sm-0 ml-auto">
-            <h3 class="font-weight-semibold">#{{$totalusers}}</h3>
-          </div>
+        <div class="col-xl-3 col-lg-6 col-sm-6">
+            <div class="widget-stat card bg-primary">
+                <div class="card-body  p-4">
+                    <div class="media">
+									<span class="mr-3">
+										<i class="la la-users"></i>
+									</span>
+                        <div class="media-body text-white">
+                            <p class="mb-1">Total Students</p>
+                            <h3 class="text-white">3280</h3>
+                            <div class="progress mb-2 bg-secondary">
+                                <div class="progress-bar progress-animated bg-light" style="width: 80%"></div>
+                            </div>
+                            <small>80% Increase in 20 Days</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  </div>
-  <div class="col-md-4">
-    <div class="card border-left-3 border-left-violet rounded-left-0">
-      <div class="card-body">
-        <div class="d-sm-flex align-item-sm-center flex-sm-nowrap">
-          <div>
-            <h6 class="font-weight-semibold">Support Ticket</h6>
-            <ul class="list list-unstyled mb-0">
-              <li>Open tickets: <span class="font-weight-semibold text-default">
-                #{{$openticket}}</span></li>
-              <li>Closed tickets: <span class="font-weight-semibold text-default">
-                #{{$closedticket}}</span>
-              </li>
-            </ul>
-          </div>
-          <div class="text-sm-right mb-0 mt-3 mt-sm-0 ml-auto">
-            <h3 class="font-weight-semibold">#{{$totalticket}}</h3>
-          </div>
+        <div class="col-xl-3 col-lg-6 col-sm-6">
+            <div class="widget-stat card bg-warning">
+                <div class="card-body p-4">
+                    <div class="media">
+									<span class="mr-3">
+										<i class="la la-user"></i>
+									</span>
+                        <div class="media-body text-white">
+                            <p class="mb-1">Total Loans</p>
+                            <h3 class="text-white">{{ $totalloan }}</h3>
+                            <div class="progress mb-2 bg-primary">
+                                <div class="progress-bar progress-animated bg-light" style="width: 50%"></div>
+                            </div>
+                            <small>50% Increase in 25 Days</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  </div>
-  <div class="col-md-4">
-    <div class="card border-left-3 border-left-violet rounded-left-0">
-      <div class="card-body">
-        <div class="d-sm-flex align-item-sm-center flex-sm-nowrap">
-          <div>
-            <h6 class="font-weight-semibold">Platform Reviews</h6>
-            <ul class="list list-unstyled mb-0">
-              <li>Published reviews: <span class="font-weight-semibold text-default">
-                #{{$pubreview}}</span></li>
-              <li>Pending reviews: <span class="font-weight-semibold text-default">
-                #{{$unpubreview}}</span>
-              </li>
-            </ul>
-          </div>
-          <div class="text-sm-right mb-0 mt-3 mt-sm-0 ml-auto">
-            <h3 class="font-weight-semibold">#{{$totalreview}}</h3>
-          </div>
+        <div class="col-xl-3 col-lg-6 col-sm-6">
+            <div class="widget-stat card bg-secondary">
+                <div class="card-body p-4">
+                    <div class="media">
+									<span class="mr-3">
+										<i class="la la-graduation-cap"></i>
+									</span>
+                        <div class="media-body text-white">
+                            <p class="mb-1">Total Course</p>
+                            <h3 class="text-white">28</h3>
+                            <div class="progress mb-2 bg-primary">
+                                <div class="progress-bar progress-animated bg-light" style="width: 76%"></div>
+                            </div>
+                            <small>76% Increase in 20 Days</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  </div>  
-  <div class="col-md-4">
-    <div class="card border-left-3 border-left-violet rounded-left-0">
-      <div class="card-body">
-        <div class="d-sm-flex align-item-sm-center flex-sm-nowrap">
-          <div>
-            <h6 class="font-weight-semibold">Other Deposits</h6>
-            <ul class="list list-unstyled mb-0">
-              <li>Pending: <span class="font-weight-semibold text-default">
-                #{{$pendingdep}}</span></li>
-              <li>Approved: <span class="font-weight-semibold text-default">
-                #{{$approveddep}}</span>
-              </li>              
-              <li>Declined: <span class="font-weight-semibold text-default">
-                #{{$declineddep}}</span>
-              </li>
-            </ul>
-          </div>
-          <div class="text-sm-right mb-0 mt-3 mt-sm-0 ml-auto">
-            <h3 class="font-weight-semibold">#{{$totaldeposit}}</h3>
-          </div>
+        <div class="col-xl-3 col-lg-6 col-sm-6">
+            <div class="widget-stat card bg-danger ">
+                <div class="card-body p-4">
+                    <div class="media">
+									<span class="mr-3">
+										<i class="la la-dollar"></i>
+									</span>
+                        <div class="media-body text-white">
+                            <p class="mb-1">Fees Collect</p>
+                            <h3 class="text-white">250$</h3>
+                            <div class="progress mb-2 bg-secondary">
+                                <div class="progress-bar progress-animated bg-light" style="width: 30%"></div>
+                            </div>
+                            <small>30% Increase in 30 Days</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>  
-  <div class="col-md-4">
-    <div class="card border-left-3 border-left-violet rounded-left-0">
-      <div class="card-body">
-        <div class="d-sm-flex align-item-sm-center flex-sm-nowrap">
-          <div>
-            <h6 class="font-weight-semibold">Bank Deposits</h6>
-            <ul class="list list-unstyled mb-0">
-              <li>Pending: <span class="font-weight-semibold text-default">
-                #{{$pendingbdep}}</span></li>
-              <li>Approved: <span class="font-weight-semibold text-default">
-                #{{$approvedbdep}}</span>
-              </li>              
-              <li>Declined: <span class="font-weight-semibold text-default">
-                #{{$declinedbdep}}</span>
-              </li>
-            </ul>
-          </div>
-          <div class="text-sm-right mb-0 mt-3 mt-sm-0 ml-auto">
-            <h3 class="font-weight-semibold">#{{$totalbdeposit}}</h3>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>  
-  <div class="col-md-4">
-    <div class="card border-left-3 border-left-violet rounded-left-0">
-      <div class="card-body">
-        <div class="d-sm-flex align-item-sm-center flex-sm-nowrap">
-          <div>
-            <h6 class="font-weight-semibold">Withdrawal</h6>
-            <ul class="list list-unstyled mb-0">
-              <li>Pending: <span class="font-weight-semibold text-default">
-                #{{$pendingwd}}</span></li>
-              <li>Approved: <span class="font-weight-semibold text-default">
-                #{{$approvedwd}}</span>
-              </li>              
-              <li>Declined: <span class="font-weight-semibold text-default">
-                #{{$declinedwd}}</span>
-              </li>
-            </ul>
-          </div>
-          <div class="text-sm-right mb-0 mt-3 mt-sm-0 ml-auto">
-            <h3 class="font-weight-semibold">#{{$totalwd}}</h3>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>  
-  <div class="col-md-4">
-    <div class="card border-left-3 border-left-violet rounded-left-0">
-      <div class="card-body">
-        <div class="d-sm-flex align-item-sm-center flex-sm-nowrap">
-          <div>
-            <h6 class="font-weight-semibold">Loan</h6>
-            <ul class="list list-unstyled mb-0">
-              <li>Payed back: <span class="font-weight-semibold text-default">
-                #{{$paidloan}}</span></li>
-              <li>Awaiting approval: <span class="font-weight-semibold text-default">
-                #{{$onholdloan}}</span>
-              </li>              
-              <li>Awaiting payback: <span class="font-weight-semibold text-default">
-                #{{$pendingloan}}</span>
-              </li>
-            </ul>
-          </div>
-          <div class="text-sm-right mb-0 mt-3 mt-sm-0 ml-auto">
-            <h3 class="font-weight-semibold">#{{$totalloan}}</h3>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>  
-  <div class="col-md-4">
-    <div class="card border-left-3 border-left-violet rounded-left-0">
-      <div class="card-body">
-        <div class="d-sm-flex align-item-sm-center flex-sm-nowrap">
-          <div>
-            <h6 class="font-weight-semibold">Py scheme plans</h6>
-            <ul class="list list-unstyled mb-0">
-              <li>Active: <span class="font-weight-semibold text-default">
-                #{{$appplan}}</span></li>
-              <li>Disabled: <span class="font-weight-semibold text-default">
-                #{{$penplan}}</span>
-              </li>              
-            </ul>
-          </div>
-          <div class="text-sm-right mb-0 mt-3 mt-sm-0 ml-auto">
-            <h3 class="font-weight-semibold">#{{$totalplan}}</h3>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>  
-  <div class="col-md-4">
-    <div class="card border-left-3 border-left-violet rounded-left-0">
-      <div class="card-body">
-        <div class="d-sm-flex align-item-sm-center flex-sm-nowrap">
-          <div>
-            <h6 class="font-weight-semibold">Py scheme investment</h6>
-            <ul class="list list-unstyled mb-0">
-              <li>Active: <span class="font-weight-semibold text-default">
-                #{{$appprofit}}</span></li>
-              <li>Completed: <span class="font-weight-semibold text-default">
-                #{{$penprofit}}</span>
-              </li>              
-            </ul>
-          </div>
-          <div class="text-sm-right mb-0 mt-3 mt-sm-0 ml-auto">
-            <h3 class="font-weight-semibold">#{{$totalprofit}}</h3>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  @stop
+    
+@stop
