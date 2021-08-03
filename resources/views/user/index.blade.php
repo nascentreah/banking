6acdeb0aaeffbb54218199f48f84524b46c7aaa6
emma ingurat <e.ingurat@bsmart-technology.com>
2021-08-03 23:44:57 +0300

    user dashboard fixes

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -123,6 +123,88 @@
         </div>
     </div>
 
+
+    <div class="row">
+        <div class="col-xl-3 col-lg-6 col-sm-6">
+            <div class="widget-stat card bg-primary">
+                <div class="card-body  p-4">
+                    <div class="media">
+									<span class="mr-3">
+										<i class="la la-users"></i>
+									</span>
+                        <div class="media-body text-white">
+                            <p class="mb-1">Total Students</p>
+                            <h3 class="text-white">3280</h3>
+                            <div class="progress mb-2 bg-secondary">
+                                <div class="progress-bar progress-animated bg-light" style="width: 80%"></div>
+                            </div>
+                            <small>80% Increase in 20 Days</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xl-3 col-lg-6 col-sm-6">
+            <div class="widget-stat card bg-warning">
+                <div class="card-body p-4">
+                    <div class="media">
+									<span class="mr-3">
+										<i class="la la-user"></i>
+									</span>
+                        <div class="media-body text-white">
+                            <p class="mb-1">Total Loans</p>
+                            <h3 class="text-white">5654333</h3>
+                            <div class="progress mb-2 bg-primary">
+                                <div class="progress-bar progress-animated bg-light" style="width: 50%"></div>
+                            </div>
+                            <small>50% Increase in 25 Days</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xl-3 col-lg-6 col-sm-6">
+            <div class="widget-stat card bg-secondary">
+                <div class="card-body p-4">
+                    <div class="media">
+									<span class="mr-3">
+										<i class="la la-graduation-cap"></i>
+									</span>
+                        <div class="media-body text-white">
+                            <p class="mb-1">Total Course</p>
+                            <h3 class="text-white">28</h3>
+                            <div class="progress mb-2 bg-primary">
+                                <div class="progress-bar progress-animated bg-light" style="width: 76%"></div>
+                            </div>
+                            <small>76% Increase in 20 Days</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-3 col-lg-6 col-sm-6">
+            <div class="widget-stat card bg-danger ">
+                <div class="card-body p-4">
+                    <div class="media">
+									<span class="mr-3">
+										<i class="la la-dollar"></i>
+									</span>
+                        <div class="media-body text-white">
+                            <p class="mb-1">Fees Collect</p>
+                            <h3 class="text-white">250$</h3>
+                            <div class="progress mb-2 bg-secondary">
+                                <div class="progress-bar progress-animated bg-light" style="width: 30%"></div>
+                            </div>
+                            <small>30% Increase in 30 Days</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <div class="row">
         <div class="col-md-6">
             <div class="card">
@@ -150,7 +232,6 @@
             </div>
         </div>
     </div>
-
 
     <div class="row">
         <div class="col-md-8">
@@ -205,6 +286,53 @@
                             </tbody>
                         </table>
                     </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-header flex-wrap pb-0">
+                    <h4 class="card-title">Make Withdraws</h4>
+                </div>
+                <div class="card-body">
+                    <form action="{{route('withdraw.submit')}}" method="post">
+                        @csrf
+                        <div class="form-group row">
+                            <label class="col-form-label col-lg-12">Amount</label>
+                            <div class="col-lg-12">
+                                <div class="input-group input-group-merge">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text">{{$currency->name}}</span>
+                                    </div>
+                                    <input type="number" step="any" name="amount" maxlength="10" class="form-control"
+                                           required/>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-form-label col-lg-12">Payout method</label>
+                            <div class="col-lg-12">
+                                <select class="form-control select" name="coin" data-dropdown-css-class="bg-primary"
+                                        data-fouc required>
+{{--                                    @foreach($method as $val)--}}
+{{--                                        <option value='{{$val->id}}'>{{$val->method}}</option>--}}
+{{--                                        @if($set->bank_withdraw==1)--}}
+{{--                                            <option value="bank">Bank</option>--}}
+{{--                                        @endif--}}
+{{--                                    @endforeach--}}
+                                </select>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-form-label col-lg-12">Details</label>
+                            <div class="col-lg-12">
+                                <textarea name="details" class="form-control" rows="4" required></textarea>
+                            </div>
+                        </div>
+                        <div class="text-right">
+                            <button type="submit" class="btn rounded mb-2 btn-success">Withdraw</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
